ed2ccbf20b8f5733a5922d44c8f5a0e2a17ea145
Joseph Isaacson <joey.a.isaacson@gmail.com>
2018-09-10 21:16:40 -0700

    some cleanup

--- app/exception_tool/exceptionCtrl.js
+++ app/exception_tool/exceptionCtrl.js
@@ -6,26 +6,30 @@
 
 angular.module('myApp.opp_module', ['ngRoute'])
 
-.controller('exceptionCtrl', function($scope) {
+.controller('exceptionCtrl', function($scope, $filter) {
 
     $scope.laneData = [{
       "pu":"Las Vegas, NV",
+      "pu_zip": 89106,
       "do": "New York, NY",
+      "do_zip": 10009,
       "coverage": true,
       "lane_volume": 22,
       "miles": 2234,
       "rate_date": "2012-04-23T18:25:43.511Z",
       "rates": [
-        {"name":"Chanview (spot)", "price": 200.23},
-        {"name":"Chanview (contract)", "price": 200.23},
-        {"name":"DAT (30 day)", "price": 200.23},
-        {"name":"DAT (90 day)", "price": 200.23},
-        {"name":"DAT (1 year)", "price": 200.23},
-        {"name":"Uber Freight", "price": 200.23}
+        {"name":"Chanview (spot)", "price": 1000.23},
+        {"name":"Chanview (contract)", "price": 1350.6},
+        {"name":"DAT (30 day)", "price": 1129.45},
+        {"name":"DAT (90 day)", "price": 1205.45},
+        {"name":"DAT (1 year)", "price": 1233.45},
+        {"name":"Uber Freight", "price": 1144.45}
       ]
     },{
       "pu":"New York, NY",
+      "pu_zip": 89106,
       "do": "Boise, ID",
+      "do_zip": 89106,
       "coverage": false,
       "lane_volume": 12,
       "miles": 1230,
@@ -40,7 +44,9 @@ angular.module('myApp.opp_module', ['ngRoute'])
       ]
     },{
       "pu":"Laredo, TX",
+      "pu_zip": 89106,
       "do": "Austin, TX",
+      "do_zip": 89106,
       "coverage": true,
       "lane_volume": 15,
       "miles": 329.5,
@@ -58,18 +64,23 @@ angular.module('myApp.opp_module', ['ngRoute'])
     // Set up some basic variables for lane adjustments
     $scope.pressedKeys = 0;
     $scope.showMarkup = false;
-    $scope.selectedMarkup = 10;
     $scope.selectedLaneVolume = 1;
     $scope.selectedLaneCoverage = 567;
     $scope.selectedLaneCoverage = 567 - ($scope.selectedLaneVolume * 1.2);
 
     // Set up some basic variables for exceptions
     $scope.selectedLane = 0;
-    $scope.selectedIndex = 4;
+    $scope.selectedIndex = 0;
+    $scope.truckCostIndex = 5;
     $scope.reasonlist = 0;
-    $scope.increments = [0.8, 0.85, 0.9, 0.95, 1, 1.05, 1.1, 1.15, 1.2];
+    $scope.markupIncrements = [0,.02,.04,.06,.08,.1,.12,.14,.16,.18,.2];
+    $scope.truckCostIncrements = [1,1.02,1.04,1.06,1.08,1.1];
     $scope.baseEstimate = 0;
     $scope.showManual = false;
+    $scope.showTruckCostAdjustment = false;
+
+    // Set up some basic calculations
+    $scope.truckCost = $scope.baseEstimate * $scope.truckCostIncrements[$scope.truckCostIndex];
 
     // Progress calculations
     function calculateProgress() {
@@ -77,28 +88,35 @@ angular.module('myApp.opp_module', ['ngRoute'])
       $scope.progPercentageStyle = {width: $scope.progPercentage + "%"};
     };
 
-    // What is this function
+    // Update the estimate
     function calculateEstimateSum() {
       // To get the average estimate from the various data sources
       // 1. Set the estimate to 0
-      $scope.baseEstimate = 0;
+      var estimateSum = 0;
       // 2. Create a variable to loop through the array
       var estPrice = $scope.laneData[$scope.selectedLane].rates;
       // 3. Create a for loop to get the sum
       var i;
       // Run a for loop to get the entire estimate
       for (i = 0; i < estPrice.length; i++) {
-        $scope.baseEstimate += estPrice[i].price;
+        estimateSum += estPrice[i].price;
       };
+      $scope.baseEstimate = estimateSum / 5;
       // Update lower / upper bounds (estimate data)
-      $scope.lowerBound = $scope.baseEstimate * $scope.increments[0];
-      $scope.upperBound = $scope.baseEstimate * $scope.increments[9];
+      $scope.lowerTruckCostBound = $scope.baseEstimate * $scope.truckCostIncrements[10];
+      $scope.upperTruckCostBound = $scope.baseEstimate * $scope.truckCostIncrements[20];
+    };
+
+    // Update the truck cost based on the selected estimate
+    function updateTruckCost() {
+      // Recalculate the truck cost estimate
+      $scope.truckCost = $scope.baseEstimate * $scope.truckCostIncrements[$scope.truckCostIndex];
     };
 
     // Compare the base estimate to the adjusted estimate
     function updateAdjustedEstimate() {
       // Recalculate the estimate data
-      $scope.selectedEstimate = $scope.baseEstimate * $scope.increments[$scope.selectedIndex];
+      $scope.selectedEstimate = $scope.truckCost * (1 + $scope.markupIncrements[$scope.selectedIndex]);
     };
 
     // Compare the base estimate to the adjusted estimate
@@ -108,8 +126,11 @@ angular.module('myApp.opp_module', ['ngRoute'])
     };
 
     function selectManualInput() {
-      var manualInput = document.getElementById('manualPrice');
-      manualInput.focus();
+      if ($scope.showManual == false) {
+        $scope.showManual = true;
+        var manualInput = document.getElementById('manualPrice');
+        manualInput.focus();
+      };
     };
 
     // Rules of what happens when a key is hit
@@ -118,15 +139,14 @@ angular.module('myApp.opp_module', ['ngRoute'])
       // Key 80 = P (Print number of keys pressed)
       if (key == 80) {
         alert("Numnber of keys pressed = " + $scope.pressedKeys);
-      // Key 81 = Q (Quick select)
-      } else if (key == 81) {
-        $scope.showManual = false;
-      // Key 67 = C (Custom entry)
-      } else if (key == 67) {
-      // Key 67 = C (Custom entry)
-        $scope.showManual = true;
-      } else if (key == 77) {
-        $scope.showMarkup = !$scope.showMarkup;
+      // Key 65 (Decrease truck cost)
+      } else if ((key == 65) && ($scope.truckCostIndex > 0)) {
+        $scope.truckCostIndex -= 1;
+        $scope.showTruckCostAdjustment = true;
+      // Key 68 = d (Increase truck cost)
+      } else if ((key == 68) && ($scope.truckCostIndex <= $scope.truckCostIncrements.length)) {
+        $scope.truckCostIndex += 1;
+        $scope.showTruckCostAdjustment = true;
       // Key 40 = down arrow (decrease reasonlist by 1)
       } else if ((key == 40) && ($scope.reasonlist <= 2)) {
         $scope.reasonlist += 1;
@@ -136,38 +156,31 @@ angular.module('myApp.opp_module', ['ngRoute'])
       // Key 13 = Enter (increase selected lane to move to next lane)
       } else if ((key == 13) && (($scope.selectedLane + 1) < ($scope.laneData.length+1))) {
         $scope.selectedLane += 1;
-        $scope.selectedIndex = 2;
+      // Key 39 = right arrow (increase selected index by 1)
+      } else if ((key == 39) && ($scope.selectedIndex < $scope.markupIncrements.length)) {
+        $scope.selectedIndex += 1;
+      // Key 39 = left arrow (decrease selected index by 1)
+      } else if ((key == 37) && ($scope.selectedIndex > 0)) {
+        $scope.selectedIndex -= 1;
       };
-      // These keys only work if manual mode is OFF
-      if ($scope.showManual == false) {
-        // Key 39 = right arrow (increase selected index by 1)
-        if ((key == 39) && ($scope.selectedIndex < 8)) {
-          $scope.selectedIndex += 1;
-        // Key 39 = right arrow (move over to manual)
-        } else if ((key == 39) && ($scope.selectedIndex == 8)) {
-          $scope.showManual = true;
-          console.log("wtf is happenings");
-        // Key 37 = left arrow (descrese selected index by 1)
-        } else if ((key == 37) && ($scope.selectedIndex > 0)) {
-          $scope.selectedIndex -= 1;
-        // Key 37 = left arrow (move over to manual)
-        } else if ((key == 37) && ($scope.selectedIndex == 0)) {
-          $scope.showManual = true;
-        }
-        // count the number of pressed keys
-        $scope.pressedKeys += 1;
-        console.log($scope.selectedIndex);
+      //If there is still some lane data then run these functions
+      if ($scope.selectedLane < $scope.laneData.length) {
+        // Update the estimate
+        calculateEstimateSum();
+        // Updat the truck cost
+        updateTruckCost();
         // Run the cummulative function
         updateAdjustedEstimate();
+        // Update progress
+        calculateProgress();
+        // Update the estimate differences
+        updateDifference();
+        //Need to run this crazy apply to update everything that was set at the top here (WTF)
       };
-      // Update progress
-      calculateProgress();
-      // Update the estimate differences
-      updateDifference();
-      //Need to run this crazy apply to update everything that was set at the top here (WTF)
+      // count the number of pressed keys
+      $scope.pressedKeys += 1;
+      $filter('number')($scope.truckCost, 0);
       $scope.$apply();
-      // Run the update manual fucntion
-      selectManualInput();
     };
 
     $scope.showAlert = function(message) {};
@@ -176,6 +189,8 @@ angular.module('myApp.opp_module', ['ngRoute'])
     calculateProgress();
     // Run the function to calculate the average estimate
     calculateEstimateSum();
+    // Update the truck cost based on the estimate
+    updateTruckCost()
     // Calculate difference between estimate and adjustment
     updateAdjustedEstimate();
     // Update the difference of estimates
